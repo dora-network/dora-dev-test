1805327209e6042a088082416372cf2c485b9f89
Kishore V <vinnakotakk@gmail.com>
2025-03-13 20:03:08 -0400

    Kafka consumer and redis data store tick processing implementation

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"dora-dev-test/data"
 	"dora-dev-test/datastore"
+	"encoding/json"
+	"fmt"
+
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
@@ -20,12 +23,19 @@ type consumer struct {
 }
 
 func (c *consumer) Save(ctx context.Context, tick data.Tick) error {
+	message := "Test Save!"
+	fmt.Println(message)
+
 	return c.ds.SaveTick(ctx, tick)
 }
 
 func (c *consumer) Start(parent context.Context) {
 	ctx, cancel := context.WithCancel(parent)
 	c.cancel = cancel
+	message := "Test!"
+	fmt.Println(message)
+	//client.ConsumeTopics("incoming_prices")
+	c.client.AddConsumeTopics("incoming_prices")
 
 	go c.start(ctx)
 }
@@ -39,6 +49,25 @@ func (c *consumer) start(ctx context.Context) {
 			// TODO implement me
 			// Consumer should consume messages from the Kafka topic using the Kafka client
 			// and save the tick data to the data store
+			fetches := c.client.PollFetches(context.Background())
+			if fetches.IsClientClosed() {
+				return
+			}
+			fetches.EachError(func(t string, p int32, err error) {
+				//die("fetch err topic %s partition %d: %v", t, p, err)
+				//message := "Test!"
+				fmt.Println("Got " + t)
+			})
+			fetches.EachRecord(func(r *kgo.Record) {
+				fmt.Print("XXX")
+				fmt.Println(r)
+				var tick data.Tick
+				err := json.Unmarshal(r.Value, &tick)
+				if err != nil {
+				}
+				c.Save(ctx, tick)
+			})
+
 		}
 	}
 }
